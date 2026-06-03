58d7b60427c629ce5952d32ecd4740720af32293
PatS4050 <patrick@straatman.eu>
2026-06-03 23:45:24 +0200

    map gemaakt, code fouten eruit gehsald. maar werkt niet

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import title from './assets/pokemonTitle.png';
 // import Button from "./components/button/Button.jsx";
 
 function App() {
-//https://pokeapi.co/api/v2/pokemon/{name}
-    const linkPokemon = 'https://pokeapi.co/api/v2/pokemon/9';
+    const linkPokemon = 'https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20';
     const [pokemon, setPokemon] = useState("");
     const [error, toggleError] = useState(false);
     const [loading, toggleLoading] = useState(false);
@@ -37,29 +36,24 @@ function App() {
                 <img src={title} alt='pokemon as title' />
                 <span><><button type='submit'>vorige</button><button type='submit'>volgende</button></></span>
                 <div>
-                    <article className="card">
-                        <h2>{pokemon.name}</h2>
-                        {/* Het ? zorgt ervoor dat, als de data nog niet bekent is, ze de rendering van de pagina stopt totdat de data wel binnen is*/}
-                        <img src={pokemon?.sprites?.front_shiny} alt='drawing of a scifi animal'/>
-                        <p><strong>Moves: </strong>{pokemon?.moves?.length}</p>
-                        <p><strong>Weight: </strong>{pokemon?.weight}</p>
-                        <ul><strong>Abilities: </strong>
-                            {pokemon?.abilities?.map((ability) => {
-                                return (<li key={ability.ability.name}>{ability.ability.name}</li>)
-                            })}
-                        </ul>
-                    </article>
-                    <article className="card">
-                        <h2>{pokemon.name}</h2>
-                        <img src={pokemon?.sprites?.front_shiny} alt='drawing of a scifi animal'/>
-                        <p><strong>Moves: </strong>{pokemon?.moves?.length}</p>
-                        <p><strong>Weight: </strong>{pokemon?.weight}</p>
-                        <ul><strong>Abilities: </strong>
-                            {pokemon?.abilities?.map((ability) => {
-                                return (<li key={ability.ability.name}>{ability.ability.name}</li>)
-                            })}
-                        </ul>
-                    </article>
+                    {pokemon.map((alien) => {
+                        return (
+                        <article className="pokemon" key={alien.abilities.id}>
+                            <h2>{alien.name}</h2>
+                            {/* Het ? zorgt ervoor dat, als de data nog niet bekent is, ze de rendering van de pagina stopt totdat de data wel binnen is*/}
+                            <img src={alien?.sprites?.front_shiny} alt='drawing of a scifi animal'
+                                 className='statue'/>
+                            <p><strong>Moves: </strong>{alien?.moves?.length}</p>
+                            <p><strong>Weight: </strong>{alien?.weight}</p>
+                            <ul><strong>Abilities: </strong>
+                                {alien?.abilities?.map((ability) => {
+                                    return (<li key={ability.ability.name}>{ability.ability.name}</li>)
+                                })}
+                            </ul>
+                        </article>
+                    )})};
+                    {loading && <h2> .... de data wordt opgehaald .... </h2>}
+                    {error && <h2> Er is iets misgegaan</h2>}
                 </div>
             </main>
         </>
